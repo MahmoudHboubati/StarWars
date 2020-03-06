1c12969b750a21ab765054cc37f179ba7f83a4bf
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2020-03-07 01:33:34 +0400

    feat(dependency): move domain dependecies to domain

---
+++ src/Domain/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using Domain.Services.Films;
+using Domain.Services.Peoples;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Domain
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDomainServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IFilmProvider, FilmProvider>();
+            services.AddScoped<IPeopleProvider, PeopleProvider>();
+
+            return services;
+        }
+    }
+}
