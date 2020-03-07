3382031716e384d46ec8dd9c0486d099aca7d092
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2020-03-07 18:32:20 +0400

    feat(most-appeared-in-speices): get appeared in most films

--- src/DAL/Repositories/SpeciesRepositories/SpeciesRepository.cs
+++ src/DAL/Repositories/SpeciesRepositories/SpeciesRepository.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using DAL.Constants;
 using DAL.Repositories.Base;
 using Domain.Entities.KeyLessEntities.SpeciesEntities;
 using Domain.Entities.SpeciesEntities;
 using Domain.Repositories.SpeciesRepositories;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using DAL.Constants;
 
 namespace DAL.Repositories.SpeciesRepositories
 {
@@ -16,8 +17,15 @@ namespace DAL.Repositories.SpeciesRepositories
 
         public async Task<IEnumerable<SpeciesAggregate>> GetMostAppeared()
         {
+            //use raw queries since groupBy (client) is not allowed in core 3.1
             var query = SqlQueries.MOST_APPEARED_SPECIES;
-            return await context.SpeciesAggregate.FromSqlRaw<SpeciesAggregate>(query).ToListAsync();
+            var mostAppearedInSpecies = await context.SpeciesAggregate.FromSqlRaw<SpeciesAggregate>(query).ToListAsync();
+            var filmsCount = await context.Films.CountAsync();
+
+            //assuming not all counts, only who appeared in most of films (most >= half)
+            var mostOfFilmsCount = filmsCount / 2;
+
+            return mostAppearedInSpecies.Where(a => a.Count >= mostOfFilmsCount);
         }
     }
 }
